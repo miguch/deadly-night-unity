64c306dc1f0042fcc4cdf1a17c719b8b4aa96fab
Miguel Chen <vvchan@outlook.com>
2022-11-06 22:41:58 -0500

    add troll death

--- Assets/SubModules/Scripts/Damage/Healthbar.cs
+++ Assets/SubModules/Scripts/Damage/Healthbar.cs
@@ -5,31 +5,35 @@ using UnityEngine.UI;
 
 namespace com.ImmersiveMedia.Damage
 {
-    /// <summary>
-    /// Provides a health bar to show the user how much health an object has remaining
-    /// </summary>
-    public class Healthbar : MonoBehaviour
+  /// <summary>
+  /// Provides a health bar to show the user how much health an object has remaining
+  /// </summary>
+  public class Healthbar : MonoBehaviour
+  {
+    [SerializeField] public bool lookAt = true;
+    [SerializeField] private Image healthBar;
+    private Camera cam;
+
+    private void Awake()
     {
-        [SerializeField] private Image healthBar;
-        private Camera cam;
-
-        private void Awake()
-        {
-            cam = FindObjectOfType<Camera>();
-        }
-
-        /// <summary>
-        /// Sets the healthbar's fil percentage
-        /// </summary>
-        /// <param name="percentage"></param>
-        public void SetHealthBarPercentage(float percentage)
-        {
-            healthBar.fillAmount = percentage;
-        }
-
-        void Update()
-        {
-            transform.LookAt(cam.transform.position); // Always look at the user in all dimensions so they can see the healthbar
-        }
+      cam = FindObjectOfType<Camera>();
     }
+
+    /// <summary>
+    /// Sets the healthbar's fil percentage
+    /// </summary>
+    /// <param name="percentage"></param>
+    public void SetHealthBarPercentage(float percentage)
+    {
+      healthBar.fillAmount = percentage;
+    }
+
+    void Update()
+    {
+      if (lookAt)
+      {
+        transform.LookAt(cam.transform.position); // Always look at the user in all dimensions so they can see the healthbar
+      }
+    }
+  }
 }
